d06ab0c773eaa8198f911dc8ead7e7a89c2f0087
khojoooo <hojoon1619@gmail.com>
2020-04-27 19:07:42 +0900

    솔리디티 수정

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ const CampaignFactory = require('./build/MailFactory.json');
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0x5534E118e66A31C452eA0aeca48019a2c76dC9c5'
+  '0x27094Fd0326987C188815dA1EF5B390eBE68C5E7'
 );
 
 module.exports = instance;
