b12892e9c74471a4fcf1aed68bf9bd48ab93e58d
khojoooo <hojoon1619@gmail.com>
2020-04-28 23:34:51 +0900

    de

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ const CampaignFactory = require('./build/MailFactory.json');
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0xcc4cF458f6217BCEa6A59FF22939fFe2A6B5dE11'
+  '0x9A665c51b0Ec72bBf3D7ab1f5637AD37E11d78Fc'
 );
 
 module.exports = instance;
